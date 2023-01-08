4e4bb759ea610d1662123c824a05d3b5e8f6103e
David Aerne <meodai@gmail.com>
2023-01-08 16:54:17 +0100

    make it work more or less

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,27 +1,29 @@
 var __pow = Math.pow;
 
 // src/index.ts
-var pointToHSL = (x, y, saturation) => {
+var pointToHSL = (x, y, z) => {
   const cy = 0.5;
   const cx = 0.5;
   const radians = Math.atan2(y - cy, x - cx);
   let deg = radians * (180 / Math.PI);
   deg = (deg + 90) % 360;
+  const s = z;
   const dist = Math.sqrt(Math.pow(y - cy, 2) + Math.pow(x - cx, 2));
   const l = dist / cx;
   return [deg, s, l];
 };
 var hslToPoint = (hsl) => {
-  const [h, s2, l] = hsl;
+  const [h, s, l] = hsl;
   const cx = 0.5;
   const cy = 0.5;
   const radians = h / (180 / Math.PI) - 90;
   const dist = l * cx;
   const x = cx + dist * Math.cos(radians);
   const y = cy + dist * Math.sin(radians);
-  return [x, y];
+  const z = s;
+  return [x, y, z];
 };
-var randomHSLPair = (minHDiff = 90, minSDiff = 0, minLDiff = 0.2, previousColor) => {
+var randomHSLPair = (minHDiff = 90, minSDiff = 0, minLDiff = 0.2, previousColor = null) => {
   let h1, s1, l1;
   if (previousColor) {
     [h1, s1, l1] = previousColor;
@@ -33,42 +35,168 @@ var randomHSLPair = (minHDiff = 90, minSDiff = 0, minLDiff = 0.2, previousColor)
   const h2 = (360 + (h1 + minHDiff + Math.random() * (360 - minHDiff * 2))) % 360;
   const s2 = minSDiff + Math.random() * (1 - minSDiff);
   const l2 = minSDiff + Math.random() * (1 - minLDiff);
-  return [[h1, s1, l1], [h2, s2, l2]];
+  return [
+    [h1, s1, l1],
+    [h2, s2, l2]
+  ];
 };
-var pointsOnLine = (p1, p2, numPoints = 4, f = (t, p) => t) => {
+var vectorsOnLine = (p1, p2, numPoints = 4, f = (t, p) => t) => {
   const points = [];
   for (let i = 0; i < numPoints; i++) {
-    let t = i / (numPoints - 1);
-    let tModified = f(t, numPoints);
-    let x = (1 - tModified) * p1.x + tModified * p2.x;
-    let y = (1 - tModified) * p1.y + tModified * p2.y;
-    points.push([x, y]);
+    const t = i / (numPoints - 1);
+    const tModified = f(t, numPoints);
+    const x = (1 - tModified) * p1[0] + tModified * p2[0];
+    const y = (1 - tModified) * p1[1] + tModified * p2[1];
+    const z = (1 - tModified) * p1[2] + tModified * p2[2];
+    points.push([x, y, z]);
   }
   return points;
 };
-var exponentialDistance = (t, n) => {
+var linearPosition = (t) => {
+  return t;
+};
+var exponentialPosition = (t) => {
   return __pow(t, 2);
 };
+var quadraticPosition = (t) => {
+  return __pow(t, 3);
+};
+var cubicPosition = (t) => {
+  return __pow(t, 4);
+};
+var quarticPosition = (t) => {
+  return __pow(t, 5);
+};
+var quinticPosition = (t) => {
+  return __pow(t, 6);
+};
+var sinusoidalPosition = (t) => {
+  return Math.sin(t * Math.PI / 2);
+};
+var reverseSinusoidalPosition = (t) => {
+  return 1 - Math.sin((1 - t) * Math.PI / 2);
+};
+var circularPosition = (t) => {
+  return 1 - Math.sqrt(1 - __pow(t, 2));
+};
+var reverseCircularPosition = (t) => {
+  return Math.sqrt(1 - __pow(1 - t, 2));
+};
+var arcPosition = (t) => {
+  return 1 - Math.sqrt(1 - t);
+};
+var reverseArcPosition = (t) => {
+  return Math.sqrt(1 - __pow(1 - t, 2));
+};
+var positionFunctions = {
+  linearPosition,
+  exponentialPosition,
+  quadraticPosition,
+  cubicPosition,
+  quarticPosition,
+  quinticPosition,
+  sinusoidalPosition,
+  reverseSinusoidalPosition,
+  circularPosition,
+  reverseCircularPosition,
+  arcPosition,
+  reverseArcPosition
+};
+var distance = (p1, p2) => {
+  const a = p2[0] - p1[0];
+  const b = p2[1] - p1[1];
+  const c = p2[2] - p1[2];
+  return Math.sqrt(a * a + b * b + c * c);
+};
+var ColorPoint = class {
+  constructor({
+    x = null,
+    y = null,
+    z = null,
+    color = null
+  } = {}) {
+    this.position = { x, y, z, color };
+  }
+  set position({
+    x = null,
+    y = null,
+    z = null,
+    color = null
+  }) {
+    if (x && y && y && color) {
+      throw new Error("Point must be initialized with either x,y,z or hsl");
+    } else if (x && y && z) {
+      this.x = x;
+      this.y = y;
+      this.z = z;
+      this.color = pointToHSL(this.x, this.y, this.z);
+    } else if (color) {
+      this.color = color;
+      [this.x, this.y, this.z] = hslToPoint(this.color);
+    }
+  }
+  get position() {
+    return [this.x, this.y, this.z];
+  }
+  get hslCSS() {
+    return `hsl(${this.color[0]}, ${this.color[1] * 100}%, ${this.color[2] * 100}%)`;
+  }
+};
 var Poline = class {
-  constructor(controlPoints, numPoints) {
-    this.pointsAsColor = controlPoints;
+  constructor(anchorColors = randomHSLPair(), numPoints = 6) {
+    this.anchorPoints = anchorColors.map(
+      (point) => new ColorPoint({ x: null, y: null, z: null, color: point })
+    );
     this.numPoints = numPoints;
-    this.points = pointsOnLine(hslToPoint(hsl1), hslToPoint(hsl2), numPoints, exponentialDistance);
+    this.points = vectorsOnLine(
+      this.anchorPoints[0].position,
+      this.anchorPoints[1].position,
+      numPoints,
+      exponentialPosition
+    ).map((p) => new ColorPoint(
+      { x: p[0], y: p[1], z: p[2], color: null }
+    ));
   }
   createPointPairs() {
     const pairs = [];
     for (let i = 0; i < this.points.length - 1; i++) {
-      pairs.push([this.points[i], this.points[i + 1]]);
+      pairs.push(
+        [this.points[i], this.points[i + 1]]
+      );
     }
     return pairs;
+  }
+  getClosestAnchorPoint(point, maxDistance) {
+    const distances = this.anchorPoints.map((anchor) => {
+      return distance(anchor.position, point);
+    });
+    const minDistance = Math.min(...distances);
+    if (minDistance > maxDistance) {
+      return null;
+    }
+    const closestAnchorIndex = distances.indexOf(minDistance);
+    return this.anchorPoints[closestAnchorIndex];
+  }
+  set anchorPoint({ pointReference, pointIndex, x, y, z, color }) {
+    let index = pointIndex;
+    if (pointReference) {
+      index = this.anchorPoints.indexOf(pointReference);
+    }
+    this.anchorPoints[index] = new ColorPoint({ x, y, z, color });
+  }
+  get colors() {
+    return this.points.map((p) => p.color);
+  }
+  get colorsCSS() {
+    return this.points.map((c) => c.hslCSS);
   }
 };
 export {
   Poline,
-  exponentialDistance,
   hslToPoint,
   pointToHSL,
-  pointsOnLine,
-  randomHSLPair
+  positionFunctions,
+  randomHSLPair,
+  vectorsOnLine
 };
 //# sourceMappingURL=index.mjs.map
--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,14 @@ export type Vector3 = [number, number, number];
 export const pointToHSL = (
   x: number,
   y: number,
-  saturation: number
+  z: number
 ): Vector3 => {
   const cy = 0.5;
   const cx = 0.5;
   const radians = Math.atan2(y - cy, x - cx);
   let deg = radians * (180 / Math.PI);
   deg = (deg + 90) % 360;
+  const s = z;
 
   const dist = Math.sqrt(Math.pow(y - cy, 2) + Math.pow(x - cx, 2));
 
@@ -20,7 +21,7 @@ export const pointToHSL = (
   return [deg, s, l];
 };
 
-export const hslToPoint = (hsl: Vector3): Vector2 => {
+export const hslToPoint = (hsl: Vector3): Vector3 => {
   const [h, s, l] = hsl;
   const cx = 0.5;
   const cy = 0.5;
@@ -29,15 +30,16 @@ export const hslToPoint = (hsl: Vector3): Vector2 => {
   const dist = l * cx;
   const x = cx + dist * Math.cos(radians);
   const y = cy + dist * Math.sin(radians);
+  const z = s;
 
-  return [x, y];
+  return [x, y, z];
 };
 
 export const randomHSLPair = (
   minHDiff = 90,
   minSDiff = 0,
   minLDiff = 0.2,
-  previousColor: Vector3
+  previousColor: Vector3 | null = null
 ): [Vector3, Vector3] => {
   let h1, s1, l1;
 
@@ -61,68 +63,205 @@ export const randomHSLPair = (
 };
 
 export const vectorsOnLine = (
-  p1: Vector2,
-  p2: Vector2,
+  p1: Vector3,
+  p2: Vector3,
   numPoints = 4,
   f = (t, p) => t
-):Vector2[] => {
-  const points:Vector2[] = [];
+):Vector3[] => {
+  const points:Vector3[] = [];
 
   for (let i = 0; i < numPoints; i++) {
     const t = i / (numPoints - 1);
     const tModified = f(t, numPoints);
-    const x = (1 - tModified) * p1.x + tModified * p2.x;
-    const y = (1 - tModified) * p1.y + tModified * p2.y;
-    points.push([x, y]);
+    const x = (1 - tModified) * p1[0] + tModified * p2[0];
+    const y = (1 - tModified) * p1[1] + tModified * p2[1];
+    const z = (1 - tModified) * p1[2] + tModified * p2[2];
+
+    points.push([x, y, z]);
   }
 
   return points;
 };
 
-export const exponentialDistance = (t: number, n: number) => {
+const linearPosition = (t: number) => {
+  return t;
+};
+
+const exponentialPosition = (t: number) => {
   return t ** 2;
 };
 
+const quadraticPosition = (t: number) => {
+  return t ** 3;
+};
+
+const cubicPosition = (t: number) => {
+  return t ** 4;
+};
+
+const quarticPosition = (t: number) => {
+  return t ** 5;
+};
+
+const quinticPosition = (t: number) => {
+  return t ** 6;
+};
+
+const sinusoidalPosition = (t: number) => {
+  return Math.sin(t * Math.PI / 2);
+};
+
+const reverseSinusoidalPosition = (t: number) => {
+  return 1 - Math.sin((1 - t) * Math.PI / 2);
+};
+
+const circularPosition = (t: number) => {
+  return 1 - Math.sqrt(1 - t ** 2);
+};
+
+const reverseCircularPosition = (t: number) => {
+  return Math.sqrt(1 - (1 - t) ** 2);
+};
+
+const arcPosition = (t: number) => {
+  return 1 - Math.sqrt(1 - t);
+};
+
+const reverseArcPosition = (t: number) => {
+  return Math.sqrt(1 - (1 - t) ** 2);
+};
+
+export const positionFunctions = {
+  linearPosition,
+  exponentialPosition,
+  quadraticPosition,
+  cubicPosition,
+  quarticPosition,
+  quinticPosition,
+  sinusoidalPosition,
+  reverseSinusoidalPosition,
+  circularPosition,
+  reverseCircularPosition,
+  arcPosition,
+  reverseArcPosition,
+};
+
+const distance = (p1, p2) => {
+  const a = p2[0] - p1[0];
+  const b = p2[1] - p1[1];
+  const c = p2[2] - p1[2];
+
+  return Math.sqrt(a * a + b * b + c * c);
+}
 class ColorPoint {
-  constructor(options: { x: number; y: number; color: Vector3 }) {
-    if (options.x && options.y && options.color) {
-      throw new Error("Point must be initialized with either x,y or hsl");
-    } else if (options.x && options.y) {
-      this.x = options.x;
-      this.y = options.y;
-      this.color = pointToHSL(this.x, this.y, 1);
-    } else if (options.color) {
-      this.color = options.color;
-      [this.x, this.y] = hslToPoint(this.color);
+  constructor({ 
+    x = null, 
+    y = null, 
+    z = null, 
+    color = null, 
+  } = {}) {
+    this.position = { x, y, z, color };
+  }
+
+  x: number | null;
+  y: number | null;
+  z: number | null;
+  color: Vector3 | null;
+
+  public set position({ 
+    x = null, 
+    y = null, 
+    z = null, 
+    color = null, 
+  }) {
+    if (x && y && y && color) {
+      throw new Error("Point must be initialized with either x,y,z or hsl");
+    } else if (x && y && z) {
+      this.x = x;
+      this.y = y;
+      this.z = z;
+      this.color = pointToHSL(this.x, this.y, this.z);
+    } else if (color) {
+      this.color = color;
+      [this.x, this.y, this.z]  = hslToPoint(this.color);
     }
   }
 
-  x:number;
-  y:number;
-  color: Vector3;
-}
-
-export class Poline {
-  constructor(controlPoints: [Vector3], numPoints: number) {
-    this.pointsAsColor = controlPoints;
-    this.numPoints = numPoints;
-    this.points = vectorsOnLine(
-      hslToPoint(hsl1),
-      hslToPoint(hsl2),
-      numPoints,
-      exponentialDistance
-    );
+  get position (): Vector3 {
+    return [this.x, this.y, this.z];
   }
 
-  pointsAsColor: Vector3[];
+  get hslCSS (): string {
+    return `hsl(${this.color[0]}, ${this.color[1] * 100}%, ${this.color[2] * 100}%)`;
+  }
+}
+
+
+export class Poline {
+  constructor(
+    anchorColors = randomHSLPair(), 
+    numPoints = 6,
+  ) {
+    this.anchorPoints = anchorColors.map(
+      point => new ColorPoint({x: null, y: null, z: null, color: point})
+    );
+
+    this.numPoints = numPoints;
+
+    this.points = vectorsOnLine(
+      this.anchorPoints[0].position,
+      this.anchorPoints[1].position,
+      numPoints,
+      exponentialPosition,
+    ).map((p) => new ColorPoint(
+      {x: p[0], y: p[1], z: p[2], color: null}
+    ));
+  }
+
+  anchorPoints: ColorPoint[];
   numPoints: number;
-  points: Vector2[];
+  points: ColorPoint[];
 
   createPointPairs() {
     const pairs = [];
     for (let i = 0; i < this.points.length - 1; i++) {
-      pairs.push([this.points[i], this.points[i + 1]]);
+      pairs.push(
+        [this.points[i], this.points[i + 1]]
+      );
     }
     return pairs;
+  }
+
+  getClosestAnchorPoint(point: Vector3, maxDistance: 1) {
+    const distances = this.anchorPoints.map((anchor) => {
+      return distance(anchor.position, point);
+    });
+
+    const minDistance = Math.min(...distances);
+    if (minDistance > maxDistance) {
+      return null;
+    }
+
+    const closestAnchorIndex = distances.indexOf(minDistance);
+    
+    return this.anchorPoints[closestAnchorIndex];
+  }
+
+  public set anchorPoint({pointReference, pointIndex, x,y,z, color}) {
+    let index = pointIndex;
+
+    if (pointReference) {
+      index = this.anchorPoints.indexOf(pointReference);
+    }
+      
+    this.anchorPoints[index] = new ColorPoint({x, y, z, color});
+  }
+
+  get colors() {
+    return this.points.map((p) => p.color);
+  }
+
+  get colorsCSS() {
+    return this.points.map((c) => c.hslCSS);
   }
 }
